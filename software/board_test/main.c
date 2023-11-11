0acbd3102f624a61eb144ef7e00c104b82dfe198
Tikitikitikidesuka <deesneakygerbil@gmail.com>
2023-11-11 17:40:55 +0100

    Update PWM library

--- software/board_test/main.c
+++ software/board_test/main.c
@@ -3,28 +3,28 @@
 #pragma config PWRTE = ON, BOR4V = BOR21V, WRT = OFF
 
 #pragma intrinsic(_delay)
-#define _XTAL_FREQ 20000000 // necessary for __delay_us
+#define _XTAL_FREQ 20000000  // necessary for __delay_us
 
-#include <xc.h>
 #include <stdio.h>
+#include <xc.h>
 
 #include "pwm.h"
 
 int max_pwm_duty_cycle_conf_value;
 
-int main()
-{
-    INTCONbits.GIE = 1; // Enable interrupts
-    
+int main() {
+    INTCONbits.GIE = 1;   // Enable interrupts
+    INTCONbits.PEIE = 1;  // Enable peripheral interrupts
+
     init_pwm();
-    set_pwm_duty_cycle(get_max_pwm_duty_cycle_conf_value());
-    
-    while(1);
+    set_pwm_period(255, 0);
+    set_pwm_duty_cycle(get_max_pwm_duty_cycle() / 2);
+
+    while (1)
+        ;
 
     return (EXIT_SUCCESS);
 }
-
-// 1010011010
 
 void __interrupt() interrupt_handler() {
     if (PIR1bits.TMR2IF) {
