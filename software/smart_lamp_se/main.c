ff7751406e0e8e6bbf6defe8539ceaf14ae0e212
Tikitikitikidesuka <deesneakygerbil@gmail.com>
2023-12-01 11:47:17 +0100

    Update main program

--- software/smart_lamp_se/main.c
+++ software/smart_lamp_se/main.c
@@ -32,28 +32,33 @@ void every_5000ms() {
 }
 
 int main() {
+    // Enable interrupts
     INTCONbits.GIE = 1;   // Enable interrupts
     INTCONbits.PEIE = 1;  // Enable peripheral interrupts
     INTCONbits.T0IE = 1;  // Enable Timer0 interrupts
-
+    
+    // Initialize ADC
+    init_adc();
+    adc = read_adc_0();
+    
+    // Initialize PWM
     init_pwm();
     set_pwm_period(255, 0);
-    set_pwm_duty_cycle(get_max_pwm_duty_cycle() / 2);
+    set_pwm_duty_cycle(0);
 
+    // Initialize timing
     init_timing();
     set_tick_duration(256 - 250, PR1to8);    // Tick = 1.6us * 250 = 400us
+    
+    // Set timing callbacks
     run_every_n_ticks(25, every_10ms);       // Every 10ms   -> 25 ticks
     run_every_n_ticks(2500, every_1000ms);   // Every 1000ms -> 2500 ticks
     run_every_n_ticks(12500, every_5000ms);  // Every 5000ms -> 12500 ticks
 
-    init_adc();
-    adc = read_adc_0();
-
-    PIE1bits.RCIE = 1;  // Disable RX interrupt
-
+    // Initialize UART
     uart_init();
     uart_set_baud_rate(32);  // 9600 bauds / sec
-    uart_puts("ABC\n");
+    PIE1bits.RCIE = 1;  // Enable RX interrupt
 
     while (1) {
         //SLEEP();
