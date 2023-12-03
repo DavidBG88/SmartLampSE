d017048c959da3d32a1d7c72ddbd4e54751debb3
Tikitikitikidesuka <deesneakygerbil@gmail.com>
2023-12-03 14:21:26 +0100

    Add i2c library

---
+++ software/smart_lamp_se/i2c.c
@@ -0,0 +1,70 @@
+// General routines for i2c comunications
+#include "i2c.h"
+
+#define i2c_ack 1
+#define i2c_nack 0
+
+#include <xc.h>
+
+void wait_MSSP(void) {
+    while (!PIR1bits.SSPIF)
+        ;
+
+    PIR1bits.SSPIF = 0;
+    return;
+}
+
+void i2c_start(void) {
+    SSPCON2bits.SEN = 1;
+    wait_MSSP();  //Wait to complete
+    return;
+}
+
+void i2c_stop(void) {
+    SSPCON2bits.PEN = 1;
+    wait_MSSP();  //Wait to complete
+    return;
+}
+
+void i2c_rstart(void) {
+    SSPCON2bits.RSEN = 1;
+    wait_MSSP();  //Wait to complete
+    return;
+}
+
+uint8_t i2c_write(uint8_t I2C_data) {
+    SSPBUF = I2C_data;
+    wait_MSSP();                             //Wait to complete
+    return ((uint8_t)!SSPCON2bits.ACKSTAT);  //Returns 1 if byte is acknowledge
+}
+
+// master_ack = 1 => acknowledge. master_ack = 0 => not acknowledge
+// Master must start (or restart) comunications first and send
+// address with the lower bit set.
+uint8_t i2c_read(uint_t master_ack) {
+    uint8_t I2C_data;
+
+    SSPCON2bits.RCEN = 1;
+    wait_MSSP();  // Wait for data
+
+    I2C_data = SSPBUF;  // It is necessary to read the buffer for SCL progress
+
+    if (master_ack)
+        SSPCON2bits.ACKDT = 0;
+    else
+        SSPCON2bits.ACKDT = 1;
+
+    SSPCON2bits.ACKEN = 1;
+    wait_MSSP();
+    return (I2C_data);
+}
+
+uint8_t i2c_write_address(uint8_t address, uint8_t read_write_bit) {
+    i2c_write(address << 1 | (read_write_bit & 1));
+}
+
+void i2c_read_n_bytes(uint8_t* bytes, uint8_t num_bytes) {
+    for (uint8_t i = 0; i < num_bytes; ++i) {
+        bytes[i] = i2c_read(I2C_ACK_OK_BIT);
+    }
+}
---
+++ software/smart_lamp_se/i2c.h
@@ -0,0 +1,36 @@
+/* 
+ * File:   i2c.h
+ * Author: norberto
+ *
+ * Created on 29 de junio de 2022, 11:43
+ */
+
+#ifndef I2C_H
+#define I2C_H
+
+#define I2C_ACK_OK_BIT 0
+#define I2C_ACK_ERR_BIT 1
+
+#define I2C_MODE_READ_BIT 1
+#define I2C_MODE_WRITE_BIT 0
+
+#include <stdint.h>
+
+void i2c_start(void);
+
+void i2c_stop(void);
+
+void i2c_rstart(void);
+
+uint8_t i2c_write(uint8_t I2C_data);
+
+uint8_t i2c_write_address(uint8_t address, uint8_t read_write_bit);
+
+// master_ack = 1 => acknowledge. master_ack = 0 => not acknowledge
+// Master must start (or restart) comunications first and send
+// address with the lower bit set.
+uint8_t i2c_read(uint8_t master_ack);
+
+void i2c_read_n_bytes(uint8_t* bytes, uint8_t num_bytes);
+
+#endif /* I2C_H */
