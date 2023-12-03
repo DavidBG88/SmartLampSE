6ffb625e3acc5195d2f00e4c8aecfabd66ebd5f6
Tikitikitikidesuka <deesneakygerbil@gmail.com>
2023-12-03 17:05:39 +0100

    Add APA102 RGB LED library

---
+++ software/smart_lamp_se/APA102.c
@@ -0,0 +1,36 @@
+#include "APA102.h"
+
+#include "spi.h"
+
+static uint8_t num_leds;
+
+static void send_start_frame() {
+    spi_write_read(0);
+    spi_write_read(0);
+    spi_write_read(0);
+    spi_write_read(0);
+}
+
+static void send_end_frame() {
+    spi_write_read(1);
+    spi_write_read(1);
+    spi_write_read(1);
+    spi_write_read(1);
+}
+
+static void send_color_frame(uint8_t power, uint8_t r, uint8_t g, uint8_t b) {
+    spi_write_read(0b11100000 | power);
+    spi_write_read(b);
+    spi_write_read(g);
+    spi_write_read(r);
+}
+
+void APA102_init(uint8_t config_num_leds) {
+    num_leds = config_num_leds;
+}
+
+void APA102_set_color(uint8_t power, uint8_t r, uint8_t g, uint8_t b) {
+    for (uint8_t i = 0; i < num_leds; ++i) {
+        send_color_frame(power, r, g, b);
+    }
+}
