3c3f270adc2ec4337f082874accaa586231e7d02
Tikitikitikidesuka <deesneakygerbil@gmail.com>
2023-12-04 20:31:25 +0100

    Fix missing start and end frame in APA102 library

--- software/smart_lamp_se/APA102.c
+++ software/smart_lamp_se/APA102.c
@@ -30,7 +30,11 @@ void APA102_init(uint8_t config_num_leds) {
 }
 
 void APA102_set_color(uint8_t power, uint8_t r, uint8_t g, uint8_t b) {
+    send_start_frame();
+
     for (uint8_t i = 0; i < num_leds; ++i) {
         send_color_frame(power, r, g, b);
     }
+
+    send_end_frame();
 }
