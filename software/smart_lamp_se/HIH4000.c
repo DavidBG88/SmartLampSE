979c03e917db1bc6ae091202c71905fa587e2314
Tikitikitikidesuka <deesneakygerbil@gmail.com>
2023-12-21 20:36:36 +0100

    Update HIH4000 library

--- software/smart_lamp_se/HIH4000.c
+++ software/smart_lamp_se/HIH4000.c
@@ -1,13 +1,16 @@
 #include "HIH4000.h"
 
-uint16_t HIH4000_read_humidity(ADCPin adc_pin) {
-    float voltage = ADC_VREF * ADC_read(adc_pin) / ADC_RESOLUTION;
-    float rh = ((voltage - V_MIN) / (V_MAX - V_MIN)) * 100.0;
+uint16_t HIH4000_read_humidity(ADCPin adc_pin, uint16_t temp_celsius) {
+    float voltage = ADC_VREF * (float)ADC_read(adc_pin) / ADC_RESOLUTION;
+    float rh = (voltage - 0.852) / 0.030;
+    float true_rh = rh / (1.0546 - (0.00216 * temp_celsius));
 
-    if (rh < 0)
-        rh = 0;
-    if (rh > 100)
-        rh = 100;
-
-    return (uint16_t)rh;
+    if (true_rh < 0)
+        true_rh = 0;
+    if (true_rh > 100)
+        true_rh = 100;
+    
+    
+    //return ADC_read(adc_pin);
+    return (uint16_t)(100 * true_rh);
 }
--- software/smart_lamp_se/HIH4000.h
+++ software/smart_lamp_se/HIH4000.h
@@ -9,14 +9,10 @@ extern "C" {
 
 #include "adc.h"
 
-#define ADC_VREF 5
-#define ADC_RESOLUTION 1024
-#define LM35_SCALE_FACTOR 10.0
-
 #define V_MIN 0.8  // Voltage at 0% RH
 #define V_MAX 3.9  // Voltage at 100% RH
 
-uint16_t HIH4000_read_humidity(ADCPin adc_pin);
+uint16_t HIH4000_read_humidity(ADCPin adc_pin, uint16_t temp_celsius);
 
 #ifdef __cplusplus
 }
